a0370d089494b9d6d85a92f3882b3bbb43f13256
Mohamed Adel Elfeky <50705405+MohamedAdelElfeky@users.noreply.github.com>
2023-09-10 18:47:38 +0300

    fixed mail

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendOtpMail;
+use App\Models\User;
+use Illuminate\Support\Facades\Cache;
 
 class ForgotPasswordController extends Controller
 {
@@ -20,5 +25,53 @@ class ForgotPasswordController extends Controller
         return $status === Password::RESET_LINK_SENT
             ? response()->json(['message' => 'Password reset link sent to your email'], 200)
             : response()->json(['message' => 'Unable to send reset link'], 400);
+    }
+
+
+    public function sendOtp(Request $request)
+    {
+        // Generate a random OTP
+        $otp = rand(1000, 9999);
+        // dd( $otp );
+        // Store the OTP in the cache for later verification
+        Cache::put('otp_' . $request->email, $otp, now()->addMinutes(10));
+
+        // Send the OTP via email
+        Mail::to($request->email)->send(new SendOtpMail($otp));
+
+        return response()->json(['message' => 'OTP sent successfully']);
+    }
+
+    public function verifyOtp(Request $request)
+    {
+        $cachedOtp = Cache::get('otp_' . $request->email);
+
+        if ($cachedOtp && $cachedOtp == $request->otp) {
+            // OTP is valid, allow the user to reset their password
+            Cache::forget('otp_' . $request->email);
+            return response()->json(['message' => 'OTP verified successfully']);
+        } else {
+            return response()->json(['message' => 'Invalid OTP'], 401);
+        }
+    }
+    
+    public function resetPassword(Request $request)
+    {
+        $cachedOtp = Cache::get('otp_' . $request->email);
+
+        if ($cachedOtp && $cachedOtp == $request->otp) {
+            // OTP is valid, allow the user to reset their password
+            Cache::forget('otp_' . $request->email);
+
+            // Reset the user's password
+            $user = User::where('email', $request->email)->first();
+            $user->update([
+                'password' => bcrypt($request->new_password),
+            ]);
+
+            return response()->json(['message' => 'Password reset successfully']);
+        } else {
+            return response()->json(['message' => 'Invalid OTP'], 401);
+        }
     }
 }
